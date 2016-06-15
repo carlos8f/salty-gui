104ad5317b236eef85a2e61bac0c5fd0d63dd7ed
Carlos Rodriguez <carlos@s8f.org>
2016-06-14 21:00:37 -0700

    wip

--- controllers/import.js
+++ controllers/import.js
@@ -1,7 +1,49 @@
+var libSalty = require('salty')
+  , request = require('micro-request')
+
 module.exports = function container (get, set) {
+  var salty = get('utils.salty')
   return get('controller')()
+    .post('/import', function (req, res, next) {
+      if (!req.user) return res.redirect('/login')
+      var input = req.body.input
+      if (input.indexOf('http') === 0) {
+        request(input, function (err, resp, body) {
+          if (err) throw err
+          if (resp.statusCode !== 200) return next(new Error('non-200 status from remote: ' + resp.statusCode))
+          if (Buffer.isBuffer(body)) body = body.toString('utf8')
+          withInput(body)
+        })
+      }
+      else withInput(input)
+      function withInput (input) {
+        try {
+          var pubkey = libSalty.pubkey.parse(input)
+        }
+        catch (e) {
+          return next(e)
+        }
+        res.vars.pubkey = pubkey
+        res.render('import-complete')
+      }
+    })
+    .post('/import/complete', function (req, res, next) {
+      if (!req.user) return res.redirect('/login')
+      salty('import', req.body.pubkey)
+        .when('Enter name: ').respond(req.body.name + '\n')
+        .when('Enter email: ').respond(req.body.email + '\n')
+        .end(function (code) {
+          if (code) return next(new Error('Import error'))
+          res.redirect('/ls')
+        })
+        .stderr.pipe(process.stderr)
+    })
     .get('/import', function (req, res, next) {
       if (!req.user) return res.redirect('/login')
+      res.on_import = true
       res.render('import')
+    })
+    .on('error', function (err, req, res) {
+      res.json(500, {err: err.message})
     })
 }
--- controllers/ls.js
+++ controllers/ls.js
@@ -1,6 +1,32 @@
+var fs = require('fs')
+  , path = require('path')
+  , crypto = require('crypto')
+  , tmpDir = require('os').tmpDir()
+  , assert = require('assert')
+  , libSalty = require('salty')
+
 module.exports = function container (get, set) {
+  var salty = get('utils.salty')
   return get('controller')()
     .get('/ls', function (req, res, next) {
-      res.render('ls')
+      if (!req.user) return res.redirect('/login')
+      res.on_ls = true
+      var chunks = []
+      salty('ls')
+        .end(function (code) {
+          if (code) return next(new Error('Wallet error'))
+          var stdout = Buffer.concat(chunks).toString('utf8')
+          var lines = stdout.split('\n').filter(function (line) {
+            return !!line
+          })
+          res.vars.recipients = lines.map(libSalty.pubkey.parse)
+          res.render('ls')
+        })
+        .stdout.on('data', function (chunk) {
+          chunks.push(chunk)
+        })
+    })
+    .on('error', function (err, req, res) {
+      res.json(500, {err: err.message})
     })
 }
