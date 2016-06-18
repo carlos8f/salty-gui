d0214c5d6caef175a5d9a2e2c22347826b30ef44
Carlos Rodriguez <carlos@s8f.org>
2016-06-18 12:04:30 -0700

    export wallet

--- controllers/save.js
+++ controllers/save.js
@@ -1,6 +1,37 @@
+var path = require('path')
+  , tmpDir = require('os').tmpDir()
+  , fs = require('fs')
+  , crypto = require('crypto')
+
 module.exports = function container (get, set) {
+  var salty = get('utils.salty')
   return get('controller')()
-    .get('/save', function (req, res, next) {
+    .post('/save', function (req, res, next) {
+      if (!req.user) return res.redirect('/login')
+      if (req.body.passphrase !== req.body.passphrase2) {
+        res.flash('Bad passphrase confirmation', 'danger')
+        return next()
+      }
+      var tmpP = path.join(tmpDir, crypto.randomBytes(32).toString('hex') + '.pem')
+      salty('save', get('conf.salty').wallet, tmpP)
+        .when('Create a passphrase: ').respond(req.body.passphrase + '\n')
+        .when('Verify passphrase: ').respond(req.body.passphrase + '\n')
+        .end(function (code) {
+          if (code) {
+            res.flash('Export error', 'danger')
+            return next()
+          }
+          get('db.tokens').make(tmpP, 'salty.pem', function (err, token) {
+            if (err) {
+              res.flash('Token error', 'danger')
+              return next()
+            }
+            res.redirect(token.url)
+          })
+        })
+    })
+    .add('/save', function (req, res, next) {
+      if (!req.user) return res.redirect('/login')
       res.render('save')
     })
 }
